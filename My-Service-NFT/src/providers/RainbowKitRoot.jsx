9f532dffeb06d100a283e02eddc7db9f35316520
root <root@ABHAY-SINGH.localdomain>
2025-11-27 09:21:28 +0000

    change all code + rainbowkit v2

--- My-Service-NFT/src/providers/RainbowKitRoot.jsx
+++ My-Service-NFT/src/providers/RainbowKitRoot.jsx
@@ -25,7 +25,8 @@ const queryClient = new QueryClient();
 const config = getDefaultConfig({
   appName: "My RainbowKit App",
   projectId: "6dd15a3684137adf8eb5ed126f061236",
-  chains: [mainnet, polygon, optimism, arbitrum, base, sepolia],
+  // chains: [mainnet, polygon, optimism, arbitrum, base, sepolia],
+  chains: [sepolia],
   ssr: true,
 });
 
