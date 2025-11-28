1edc0f03f7e0f4b1f40c63bab131479aa37ad7f0
Farman <110935830+farman13@users.noreply.github.com>
2025-11-28 22:52:38 +0530

    Remove mainnet from RainbowKit chains configuration

--- My-Service-NFT/src/providers/RainbowKitRoot.jsx
+++ My-Service-NFT/src/providers/RainbowKitRoot.jsx
@@ -23,7 +23,7 @@ const queryClient = new QueryClient();
 const config = getDefaultConfig({
   appName: "My RainbowKit App",
   projectId: "6dd15a3684137adf8eb5ed126f061236",
-  chains: [mainnet, polygon, optimism, arbitrum, base, sepolia],
+  chains: [polygon, optimism, arbitrum, base, sepolia],
   ssr: true,
 });
 
