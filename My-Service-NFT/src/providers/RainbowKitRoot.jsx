16a43e81b702d4767b89fb84fc2cf4df7d267653
root <root@ABHAY-SINGH.localdomain>
2025-11-29 20:07:33 +0000

    updaing wagmi config  project id

--- My-Service-NFT/src/providers/RainbowKitRoot.jsx
+++ My-Service-NFT/src/providers/RainbowKitRoot.jsx
@@ -20,12 +20,37 @@ import {
 
 const queryClient = new QueryClient();
 
+    // const sepoliaWithRpc = {
+    //   ...sepolia,
+    //   rpcUrls: {
+    //     default: {
+    //       http: ["https://eth-sepolia.g.alchemy.com/v2/https://eth-sepolia.g.alchemy.com/v2/uPRzvfSgWOxMDJ6LJQGAO"], // ⭐ your fast RPC
+    //     },
+    //     public: {
+    //       http: ["https://eth-sepolia.g.alchemy.com/v2/https://eth-sepolia.g.alchemy.com/v2/uPRzvfSgWOxMDJ6LJQGAO"],
+    //     },
+    //   },
+    // };
+    //6dd15a3684137adf8eb5ed126f061236
+
 const config = getDefaultConfig({
   appName: "MyService NFT",
   projectId: "bf59cafc9ab6aee1a645b92a22cf252e",
   chains: [mainnet, polygon, optimism, arbitrum, base, sepolia],
+
+  walletConnectOptions: {
+    projectId: "bf59cafc9ab6aee1a645b92a22cf252e",
+    metadata: {
+      name: "MyService NFT",
+      description: "Service Lottery + NFT dApp",
+      url: "https://www.my10years.space", // <-- IMPORTANT!
+      
+    }
+  },
+
   ssr: true,
 });
+
 
 export function RainbowKitRoot({ children }) {
   return (
